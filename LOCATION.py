f608812bc526009801998b785c412e4bc7afca40
Michael Cornell <77296076+MicCorn@users.noreply.github.com>
2022-11-28 18:56:44 +0000

    EVERYTHNGH

--- LOCATION.py
+++ LOCATION.py
@@ -1,17 +1,29 @@
-def search(customer_location):
-  foodDic = {"r1" : 15, "r2" : 20
-    
-  }
-  return foodDic
-
-
 import googlemaps
 from datetime import datetime
-
 gmaps = googlemaps.Client(key='AIzaSyDlf5AW-SAArmbJNIQieRH84zv_I2Iugr4')
 
-# Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+def search(customer_location):
+  foodDic = {}
+  if customer_location == len(customer_location)*" " or customer_location == "here":
+    locTuple = (41.38843845116738, 2.170912691748747)
+  else:
+    geocode_result = gmaps.geocode(customer_location)
+    if geocode_result == []:
+      raise NameError("Could not find " + customer_location)
+    locTuple = (geocode_result[0]["geometry"]["location"]["lat"], geocode_result[0]["geometry"]["location"]["lng"])
+  awful_dict = gmaps.places_nearby(location = locTuple, radius = 15000, keyword = "kebab", open_now = True, type = "restaurant")
+  rest = 0
+  for item in awful_dict["results"]:
+    rest +=1
+    try:
+      foodDic["r" + str(rest)] = item["name"], item["rating"], item["price_level"]
+    except KeyError:
+      foodDic["r" + str(rest)] = item["name"], item["rating"]
+  
+  return foodDic
+"""
+print(item["name"], "--- Rating:", item["rating"], "--- Price Rating:", item["price_level"])
+print(item["name"], "--- Rating:", item["rating"])
 
 # Look up an address with reverse geocoding
 reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
@@ -30,7 +42,7 @@ addressvalidation_result =  gmaps.addressvalidation(['1600 Amphitheatre Pk'],
                                                     regionCode='US',
                                                     locality='Mountain View', 
                                                     enableUspsCass=True)
-  
+  """
 
   
   
